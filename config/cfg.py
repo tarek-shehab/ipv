03c985a984e68a1cd1847d3438ebfb373132aee6
tarek <alex.kovalsky@taikitech.com>
2018-07-25 13:54:09 -0400

    Global config have been commited

---
+++ config/cfg.py
@@ -0,0 +1,36 @@
+######################################################################
+#
+######################################################################
+active_models = {'ipg': [
+#                         'main',
+#                         'assignee',
+#                         'inventor',
+#                         'd_inventor',
+#                         'applicant',
+#                         'claims',
+#                         'classification',
+#                         'agent',
+                          'provisional',
+                          'related',
+                        ],
+#                 'ad': ['assignments', 'a_assignee'],
+#                 'att': ['attorney']
+                }
+
+active_parsers = {'ipg': [
+#                         'main',
+#                         'assignee',
+#                         'inventor',
+#                         'd_inventor',
+#                         'applicant',
+#                         'claims',
+#                         'classification',
+#                         'agent',
+                          'provisional',
+                          'related',
+                        ],
+#                 'ad': ['assignments', 'a_assignee'],
+#                 'att': ['attorney']
+                }
+
+hdfs_base_dir = '/ipv'
