1f00ecef9b41cbca87db694cb1c386a6200d9c34
tarek <alex.kovalsky@taikitech.com>
2018-07-30 11:15:13 -0400

    IPA Parser has been added

---
+++ ipa_start.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#############################################################################
+#
+#############################################################################
+import sys
+import time
+import logging
+import os
+from os import walk
+import routines.dbs_handlers as dbs
+import routines.tbl_handlers as tbl
+import routines.wpr_handlers as parser
+
+#############################################################################
+if __name__ == "__main__":
+
+    logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s',
+                        level=logging.INFO)
+
+    source_dir = './source/ipa/'
+    processed_dir = './source/processed/ipa/'
+
+    ftime = time.time()
+    parser.set_env()
+    if dbs.init_dbs():
+
+        flist = ['ipa100107.xml']
+#        flist = []
+#        for (dirpath, dirnames, filenames) in walk(source_dir):
+#            flist.extend(filenames)
+#            break
+
+        logging.info(('Found %s files to process') % (len(flist)))
+        for fl in flist:
+            stime = time.time()
+            if tbl.load_tables(parser.parse(source_dir + fl)):
+                if not os.path.exists(processed_dir):
+                    os.makedirs(processed_dir)
+                os.rename(source_dir + fl, processed_dir + fl)
+                logging.info(('File %s successfully parsed and loaded into Impala table in %s sec.') % (fl,
+                              str(round(time.time()-stime,2))))
+            else:
+                logging.error(('Failed to process %s') % (fl))
+                quit(1)
+
+        logging.info(('Total processing time: %s') % (str(round(time.time()-ftime,2))))
+    else:
+        logging.error('Failed to initialize Databases')
--- parsers/ipa/applicant.py
+++ parsers/ipa/applicant.py
@@ -22,6 +22,6 @@ def create_line(xml_part):
 
     app_num_tag = ".//application-reference/document-id/doc-number"
 
-    parts_tag = ".//us-applicants/us-applicant"
+    parts_tag = ".//applicants/applicant"
 
     return helpers.extract(xml_part, to_extract, parts_tag, app_num_tag)
